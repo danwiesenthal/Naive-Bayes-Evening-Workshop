b5166d87bef64c53de8494101c65248ca25fe5a2
Daniel Wiesenthal <dw@cs.stanford.edu>
2017-08-31 10:24:53 -0700

    PEP whitespace style fix

--- classifiers.py
+++ classifiers.py
@@ -8,6 +8,7 @@ import functools
 #  Advanced material here, feel free to ignore memoize if you like.  Long story short, it remembers the inputs and outputs to functions, and if the same input is seen multiple times, then rather than running the function multiple times, memoization just returns the result of the function when it was first called with that input.  Memory expensive because it keeps track of past results, but computationally nice because we don't recalculate the same thing over and over.
 def memoize(obj):
     cache = obj.cache = {}
+
     @functools.wraps(obj)
     def memoizer(*args, **kwargs):
         if args not in cache:
