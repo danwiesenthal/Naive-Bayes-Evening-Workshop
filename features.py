cdc3fd4794682f879f8c69725d73a0836c73c5eb
Daniel Wiesenthal <dw@cs.stanford.edu>
2017-10-24 15:45:23 -0700

    add another bonus :)

--- features.py
+++ features.py
@@ -6,6 +6,7 @@ PUNCTUATION = set(string.punctuation)
 
 
 def _simple_featurize_text(text):
+    '''Bonus (beginner): extend _simple_featurize_text() to remove common words, known as stopwords, like "the" and "and" since it's generally found that they don't add much signal. What other words might make sense to remove (because they don't add signal) in the context of news articles or online comments?  (Hint: try thinking both about words that occur so commonly that they have little signal, as well as words that occur super rarrrrrely and therefore might nottt havve siggnal ;) '''
     featuredict = Counter()
     for t in text.split():  # split on whitespace
         key = t.lower()  # lowercase
@@ -15,7 +16,7 @@ def _simple_featurize_text(text):
 
 
 def featurize_text(text):
-    '''Bonus: extend featurize_text() to do more than just what's in
+    '''Bonus (intermediate): extend featurize_text() to do more than just what's in
     _simple_featurize_text(). For example, you might start by excluding
     stopwords. A next step might be adding support for bigrams ("hot dog")
     instead of just unigrams ("hot", "dog"). What else can you think of?
